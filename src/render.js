adf3952dcbddc86f785369eb249d7c920790c64e
Feilding Cage <feilding.cage@gmail.com>
2017-05-09 16:54:15 +0100

    creates project to upload

--- src/render.js
+++ src/render.js
@@ -1,5 +1,28 @@
 import mainTemplate from './src/templates/main.html!text'
+//import headerTemplate from './src/templates/header.html!text'
+import Mustache from 'mustache'
+import rp from 'request-promise'
 
-export async function render() {
-    return mainTemplate;
+var partialTemplates = {
+	//"header": headerTemplate
+};
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/1vXN9tAzU2_UQUulBW3UMAyNtBfI4fj23csgteZEQYF4.json',
+        json: true
+    }).then((data) => {
+      //console.log(data.sheets.Sheet1)
+      var d = groupBy(data.sheets.Sheet1, 'Area');
+      
+      var html = Mustache.render(mainTemplate, {topics: d}, partialTemplates);
+      return html;
+    });
 }
+
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
