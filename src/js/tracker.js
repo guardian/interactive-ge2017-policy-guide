4a1a63b5aefbbdce6ddb42534cc03ed5406c070e
Feilding Cage <feilding.cage@gmail.com>
2017-05-18 13:31:07 +0100

    adds tracking

---
+++ src/js/tracker.js
@@ -0,0 +1,40 @@
+export default function tracker(){
+
+  var w = window.parent;
+  var isTracking = (w.ga) ? true : false;
+  var initialized = false;
+  var events = {};
+
+  function registerEvent(e, data){
+    //console.log('event fired')
+    //console.log("isTracking", isTracking, 'isInitialized', initialized);
+    //is tracking
+    if(isTracking){
+
+      //make sure initialized
+      if(!initialized){
+        //console.log('init tracking')
+        w.ga('create', 'UA-78705427-1', 'auto');
+        w.ga('set', 'dimension3', 'theguardian.com' );
+        initialized = true;
+      }
+      //send event
+
+      if( e === 'stack_card_view'){
+
+        if( !events[e]){
+          events[e] = 0;
+        }
+        events[e] += 1;
+        data = data + '_' + events[e];
+      }
+
+      w.ga("send", "event", "interactives", e, data);
+    }
+  }
+
+  return {
+    registerEvent: registerEvent
+  }
+
+}
